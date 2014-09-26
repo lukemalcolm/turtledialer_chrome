5bca70ad24b04a9ef8c6603048455b2900776146
Francesco Faraone <ffaraone@gaa9.com>
2014-09-26 18:59:25 +0200

    wip

--- js/background2.js
+++ js/background2.js
@@ -1,4 +1,22 @@
-chrome.browserAction.disable();
+/*
+Copyright 2014 Francesco Faraone
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/********************************************
+ Global variables
+*********************************************/
 
 var config = new Config();
 var phone = null;
@@ -102,6 +120,7 @@ var retrieveContacts = function() {
  Extension function
 *********************************************/
 var initialize = function() {
+	chrome.browserAction.disable();
 	if (config.check()) {
 		phone_class = config.getSettings().phone;
 		phone = new this[phone_class](config.getSettings());
@@ -111,8 +130,8 @@ var initialize = function() {
 }
 var start = function() {
 	chrome.browserAction.enable();
-	chrome.browserAction.setBadgeText({text: '' + Object.keys(contacts).length});
-	chrome.browserAction.setBadgeBackgroundColor({color: '#00cc00'});
+	// chrome.browserAction.setBadgeText({text: '' + Object.keys(contacts).length});
+	// chrome.browserAction.setBadgeBackgroundColor({color: '#00cc00'});
 	chrome.contextMenus.create({
 		title: chrome.i18n.getMessage('mnu_dial'),
 		contexts: ["selection"],
