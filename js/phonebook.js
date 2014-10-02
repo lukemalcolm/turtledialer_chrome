6dae5af85000778b9776ebd26d155e591c1abc9b
Francesco Faraone <ffaraone@gaa9.com>
2014-10-03 00:40:09 +0200

    no message

--- js/phonebook.js
+++ js/phonebook.js
@@ -31,7 +31,7 @@ $(function() {
 			if (msg != undefined && msg != '') {
 				$(this).text(msg);
 			}
-			
+
 		}
 	);
 	chrome.extension.getBackgroundPage().resetMissedCallsCount();
@@ -57,18 +57,18 @@ $(function() {
 			$('<div>').addClass('col-xs-4')
 		];
 		for (var j = 0; j < curr_contact['numbers'].length; j++) {
-			var col_to_add = cols[j%3];
+			var col_to_add = cols[j % 3];
 			var current_number = curr_contact['numbers'][j];
-				var icon_class = 'fa fa-phone';
-				if (icons.hasOwnProperty(current_number['kind'])) {
-					icon_class = icons[current_number['kind']];
-				}
+			var icon_class = 'fa fa-phone';
+			if (icons.hasOwnProperty(current_number['kind'])) {
+				icon_class = icons[current_number['kind']];
+			}
 			col_to_add.append($('<i>').addClass(icon_class));
 			var num = $('<a>').addClass('number-link').attr('href', '#');
-			var phone_number = 
+			var phone_number =
 				chrome.extension.getBackgroundPage().number_utils.formatPhoneNumber(
 					current_number['number']
-			);
+				);
 			num.text(phone_number);
 			col_to_add.append(num);
 			col_to_add.append($('<br>'));
@@ -77,7 +77,9 @@ $(function() {
 		body.append(cols);
 		$('#data').append(li);
 	}
-	$('#data').btsListFilter('#searchinput', {itemChild: 'h4'});
+	$('#data').btsListFilter('#searchinput', {
+		itemChild: 'h4'
+	});
 
 	var calls_log = chrome.extension.getBackgroundPage().calls_log;
 	for (var i = 0; i < calls_log.length; i++) {
@@ -91,7 +93,9 @@ $(function() {
 		td.text(calls_log[i]['date'] + ' ' + calls_log[i]['time']);
 		tr.append(td);
 		td = $('<td>').addClass('col-who');
-		td.text(calls_log[i]['name']);
+		p = $('<p>');
+		td.append(p);
+		p.text(calls_log[i]['name']);
 		tr.append(td);
 		td = $('<td>').addClass('col-number');
 		var num = $('<a>').addClass('number-link').attr('href', '#');
@@ -100,14 +104,25 @@ $(function() {
 		tr.append(td);
 		$('#tbl-log').append(tr);
 	}
-	
+
 	$('.number-link').click(function() {
 		console.log('try dialing  ' + $(this).text());
-		chrome.extension.getBackgroundPage().dial($(this).text());	
+		chrome.extension.getBackgroundPage().dial($(this).text());
 	});
 
-	$('#tab-links a').click(function (e) {
-  		e.preventDefault()
-  		$(this).tab('show')
+	$('#tab-links a').click(function(e) {
+		e.preventDefault()
+		$(this).tab('show')
+	});
+	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+		if ($(e.target).attr('href') == '#pb-tab') {
+			$('#data').btsListFilter('#searchinput', {
+				itemChild: 'h4'
+			});
+		} else {
+			$('#tbl-log').btsListFilter('#searchinput', {
+				itemChild: 'p'
+			});
+		}
 	});
 });
